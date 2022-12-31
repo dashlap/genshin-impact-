e263b3f75bc6354212f40d46b310318f1d9b04e2
dashlap <94745136+dashlap@users.noreply.github.com>
2022-12-31 20:43:53 +0400

    Add files via upload

---
+++ Genshin-Impact-main/js/index.js
@@ -0,0 +1,61 @@
+const btnOff = $('.header-toggle'),
+	btnOn = $('.close-btn'),
+    main = $('body'),
+    landing = $(window);
+
+let pagePosition = 0;
+
+let disableScroll = function () {
+    pagePosition = landing.scrollTop();
+    main.addClass('disable-scroll');
+}
+
+let enableScroll = function () {
+    main.removeClass('disable-scroll');
+    landing.scrollTop(pagePosition);
+}
+
+// Закрытие Модально окна через Кнопку Крестик
+btnOn.on('click', (e) => {
+    $('.mobile-modal-menu').hide();
+    enableScroll();
+});
+
+// Открытие Модального окна через кнопку
+btnOff.on('click', (e) => {
+    $('.mobile-modal-menu').show();
+    disableScroll();
+});
+
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    centeredSlides: true,
+    loop: true,
+    autoplay: {
+      delay: 3500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+});
+
+
+let active = 0;
+$('.weapon-all').on('click', function() {
+  
+  $($('.weapon-all')[active]).removeClass('active-all');
+  $(this).addClass('active-all');
+  
+  $($('.question')[active]).removeClass('active');
+  $($('.question')[$(this).attr('id')]).addClass('active');
+
+  active = $(this).attr('id');
+});
